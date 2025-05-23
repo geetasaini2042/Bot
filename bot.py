ce8a905c7aa408fa429aec83e10ac31e09ea77c9
geetasaini2042 <Geetasaini2042@gmail.com>
2025-05-23 10:49:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,10 +31,42 @@ def run_bot():
     app.run()
 
 # Pyrogram handler
+
 @app.on_message(filters.command("start") & filters.private)
 async def start_handler(client, message):
-    await message.reply_text("Welcome to the bot!")
+    text = (
+        "Welcome to *SingodiyaTech Result Bot!*\n\n"
+        "Check your Rajasthan Board Result for 10th & 12th in one click.\n\n"
+        "*Steps:*\n"
+        "1. Tap the button below\n"
+        "2. Enter your roll number\n"
+        "3. Get your marks instantly as PDF or online view.\n"
+        "Use /help command to get help.\n"
+        "If current server getting failed. Please use /Server2 Command."
+    )
 
+    keyboard = {
+        "inline_keyboard": [
+            [
+                {
+                    "text": "10th Result 2025",
+                    "web_app": {"url": "https://geetasaini2042.github.io/Results/RAJ/2025/10th/"}
+                }
+            ],
+            [
+                {
+                    "text": "12th Result 2025",
+                    "web_app": {"url": "https://geetasaini2042.github.io/Results/RAJ/2025/12th/"}
+                }
+            ]
+        ]
+    }
+
+    await message.reply_text(
+        text,
+        parse_mode="markdown",
+        reply_markup=keyboard
+    )
 # Entry point
 if __name__ == "__main__":
     flask_thread = threading.Thread(target=run_flask)
